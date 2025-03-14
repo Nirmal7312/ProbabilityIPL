73e830b44cdadf418388de539b405f6b4fe8e86b
Nirmal <aman.kumar@ncfe.ac.in>
2025-03-14 22:48:22 +0530

    initial

---
+++ ProbabilityIPL/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index (request):
+
+    team1 = request.POST.get('text', 'default')
+    team2 = request.POST.get('team2', 'default')
+    pitch = request.POST.get('pitch', 'default')
+    t1spinners = request.POST.get('t1spinners', 'default')
+    t1pacers = request.POST.get('t1pacers', 'default')
+    t1allrounders = request.POST.get('t1allrounders', 'default')
+    t1captain = request.POST.get('t1captain', 'default')
+    t2spinners = request.POST.get('t2spinners', 'default')
+    t2pacers = request.POST.get('t2pacers', 'default')
+    t2allrounders = request.POST.get('t2allrounders', 'default')
+    t2captain = request.POST.get('t2captain', 'default')
+    return render(request, 'index.html')
